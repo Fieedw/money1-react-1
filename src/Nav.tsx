757073ed01b4016fb2c9510e02871064e1fe59e6
fieedw <frankding8843@gmail.com>
2020-06-12 01:19:22 +0800

    创建Icon组件

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+import Icon from './Icon';
 
 const Nav = styled.nav`
 line-height:24px;
@@ -10,25 +11,35 @@ box-shadow: 0 0 3px rgba(0,0,0,0.25);
   > li {
   width: 33.333%;
   text-align: center;
-  padding: 16px;
+  display: flex;
+  flex-direction: column;
+   justify-content:center;
+  align-items: center;
+  .icon{
+  width:32px;
+  height:32px;
+  }
    }
   }
-`
-const nav =()=>{
-  return(
+`;
+const nav = () => {
+  return (
     <Nav>
       <ul>
         <li>
+          <Icon name="money"/>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <Icon name="label"/>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <Icon name="statistics"/>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
     </Nav>
-  )
-}
+  );
+};
 export default nav;
