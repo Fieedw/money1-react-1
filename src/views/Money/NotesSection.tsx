6798573fc998a403ec2d43c462e2eaedaff962e9
fieedw <frankding8843@gmail.com>
2020-06-23 16:04:05 +0800

    非受控组件变成受控组件

--- src/views/Money/NotesSection.tsx
+++ src/views/Money/NotesSection.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler, useRef} from 'react';
 import {Input} from '../../components/Input';
 
 const Wrapper = styled.section`
@@ -7,21 +7,18 @@ background: #f5f5f5;padding: 0 16px;font-size:14px;`;
 type Props ={value: string;onChange:(value: string)=>void}
 const NotesSection: React.FC<Props> = (props) => {
   const note = props.value;
-  const refInput = useRef<HTMLInputElement>(null);
-  const onBlur = () => {
-    if (refInput.current !== null) {
-      props.onChange(refInput.current.value);
-    }
+  const onChange:ChangeEventHandler<HTMLInputElement> = (e) => {
+      props.onChange(e.target.value);
   };
   return (
     <Wrapper>
-      <Input>
+      <Input label="备注" type="text" value={note} onChange={onChange}>
         {/*<span>备注</span>*/}
         {/*<input type="text" placeholder="在这里添加备注"*/}
-          //受控组件
-          // value={note}
-          // onChange={(e) => setNote(e.target.value)}
-          //非受控组件
+        {/*  //受控组件*/}
+        {/*  // value={note}*/}
+        {/*  // onChange={(e) => setNote(e.target.value)}*/}
+        {/*  //非受控组件*/}
         {/*       ref={refInput}*/}
         {/*       defaultValue={note}*/}
         {/*       onBlur={onBlur}*/}
