fc658cefc87b7001068d532a83b29523c55b982f
fieedw <frankding8843@gmail.com>
2020-06-23 17:20:00 +0800

    删除tag

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,44 +1,46 @@
 import React from 'react';
 import {useTags} from '../useTags';
-import {useParams} from 'react-router-dom'
+import {useParams} from 'react-router-dom';
 import Layout from '../components/Layout';
 import Icon from '../components/Icon';
 import {Button} from '../components/Button';
 import styled from 'styled-components';
 import {Input} from '../components/Input';
-import { Center } from 'components/Center';
+import {Center} from 'components/Center';
 import {Space} from '../components/Space';
 
-type Params ={
-  id:string
+type Params = {
+  id: string
 }
-const TopBar =styled.header`
+const TopBar = styled.header`
 display:flex;justify-content: space-between;align-items:center;line-height: 20px;padding: 14px;background:white;
 > span{font-size:16px;}
-`
-const InputWrapper =styled.div`
+`;
+const InputWrapper = styled.div`
 background: white;padding: 0 16px;margin-top: 16px;
-`
+`;
 const Tag: React.FC = () => {
-  const {findTag,updateTag} =useTags()
-  let {id:idString} =useParams<Params>()
-  const tag = findTag(parseInt(idString))
+  const {findTag, updateTag, deleteTag} = useTags();
+  let {id: idString} = useParams<Params>();
+  const tag = findTag(parseInt(idString));
   return (
     <Layout>
-<TopBar>
-  <Icon name="left"/>
-  <span>编辑标签</span>
-  <span> </span>
-</TopBar>
-      <InputWrapper>
-        <Input label="标签名" type="text" placeholder="请输入新的标签名" value={tag.name}
-               onChange={(e)=>{updateTag(tag.id,{name: e.target.value})}} />
-      </InputWrapper>
-      <Center>
-        <Space/>
-        <Space/>
-        <Button>删除标签</Button>
-      </Center>
+      <TopBar>
+        <Icon name="left"/>
+        <span>编辑标签</span>
+        <span> </span>
+      </TopBar>
+      {
+        tag ? <div><InputWrapper>
+          <Input label="标签名" type="text" placeholder="请输入新的标签名" value={tag.name}
+                 onChange={(e) => {updateTag(tag.id, {name: e.target.value});}}/>
+        </InputWrapper>
+          <Center>
+            <Space/>
+            <Space/>
+            <Button onClick={() => deleteTag(tag.id)}>删除标签</Button>
+          </Center></div> : <Center>tag不存在</Center>
+      }
     </Layout>
   );
 };
