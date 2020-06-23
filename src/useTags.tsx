68bc64209f98e01270b610cb41970efa4cbc7d87
fieedw <frankding8843@gmail.com>
2020-06-23 17:28:42 +0800

    简化读写实现方法

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,39 +1,41 @@
 import {useState} from 'react';
 import {createId} from './lib/createld';
 
-const defaultTags =[
-  {id:createId(),name:'衣'},
-  {id:createId(),name:'食'},
-  {id:createId(),name:'住'},
-  {id:createId(),name:'行'}
-]
+const defaultTags = [
+  {id: createId(), name: '衣'},
+  {id: createId(), name: '食'},
+  {id: createId(), name: '住'},
+  {id: createId(), name: '行'}
+];
 const useTags = () => {//封装一个自定义 Hook
-  const [tags, setTags] = useState<{id: number,name: string}[]>(defaultTags);
-  const findTag = (id: number) => tags.filter(tag => tag.id === id)[0]
-  const findTagIndex =(id:number) =>{
+  const [tags, setTags] = useState<{ id: number, name: string }[]>(defaultTags);
+  const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
+  const findTagIndex = (id: number) => {
     let result = -1;
-    for (let i=0;i< tags.length;i++){
-      if(tags[i].id===id){
-        result =i;
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[i].id === id) {
+        result = i;
         break;
       }
     }
-    return result
-  }
-  const updateTag =(id:number,obj:{name:string})=>{
-    const index =findTagIndex(id)
-    const tagsClone =JSON.parse(JSON.stringify(tags))
-    tagsClone.splice(index,1,{id:id,name:obj.name})
-    setTags(tagsClone)
-  }
-  const deleteTag =(id:number)=>{
-    const index =findTagIndex(id)
-    const tagsClone =JSON.parse(JSON.stringify(tags))
-    tagsClone.splice(index,1)
-    setTags(tagsClone)
-  }
-  return{
-    tags,setTags,findTag,findTagIndex,updateTag,deleteTag
-  }
+    return result;
+  };
+  const updateTag = (id: number, obj: { name: string }) => {
+    setTags(tags.map(tag => tag.id === id ? {id, name:obj.name} : tag));
+    // const index =findTagIndex(id)
+    // const tagsClone =JSON.parse(JSON.stringify(tags))
+    // tagsClone.splice(index,1,{id:id,name:obj.name})
+    // setTags(tagsClone)
+  };
+  const deleteTag = (id: number) => {
+    setTags(tags.filter(tag => tag.id !== id))
+    // const index = findTagIndex(id);
+    // const tagsClone = JSON.parse(JSON.stringify(tags));
+    // tagsClone.splice(index, 1);
+    // setTags(tagsClone);
+  };
+  return {
+    tags, setTags, findTag, findTagIndex, updateTag, deleteTag
+  };
 };
 export {useTags};
